0ae89d08bcac06d4abec034df305d1d1c046a638
DavidSnodgrass1 <dcs0036@mix.wvu.edu>
2021-08-27 09:32:14 -0400

    lab 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SecondTopNavBarComponent } from './header/2nd-top-nav-bar-component';
+import { TopNavBarComponent } from './header/top-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    SecondTopNavBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/2nd-top-nav-bar-component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-second-top-nav-bar',
+    templateUrl: '2nd-top-nav-bar-component.html'
+})
+export class SecondTopNavBarComponent {
+
+}

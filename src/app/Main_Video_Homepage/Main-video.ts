d6b79c88b358aa4bc3fe97ca920f7d4c3aba142b
DavidSnodgrass1 <dcs0036@mix.wvu.edu>
2021-09-02 18:42:38 -0400

    in development lab 2

---
+++ src/app/Main_Video_Homepage/Main-video.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-main-video',
+    templateUrl: 'Main-Video.html',
+    styleUrls: ['./main-video.css']
+})
+export class MainVideo {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainVideo } from './Main_Video_Homepage/Main-video';
+
 import { SecondTopNavBarComponent } from './header/2nd-top-nav-bar-component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
 
@@ -9,7 +11,8 @@ import { TopNavBarComponent } from './header/top-nav-bar.component';
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    SecondTopNavBarComponent
+    SecondTopNavBarComponent,
+    MainVideo
   ],
   imports: [
     BrowserModule

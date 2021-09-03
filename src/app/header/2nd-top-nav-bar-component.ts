e11bf3366333eac999c28be2888ef6c6a25eb005
DavidSnodgrass1 <dcs0036@mix.wvu.edu>
2021-09-03 01:22:49 -0400

    touch ups on the components and fromat changes

--- src/app/header/2nd-top-nav-bar-component.ts
+++ src/app/header/2nd-top-nav-bar-component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-second-top-nav-bar',
-    templateUrl: '2nd-top-nav-bar-component.html'
+    templateUrl: '2nd-top-nav-bar-component.html',
+    styleUrls: ['./2nd-top-nav-bar-component.css']
 })
 export class SecondTopNavBarComponent {
 

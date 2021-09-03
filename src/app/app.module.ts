3c1d179a186b5fec4c34b55f82891df6002eee44
DavidSnodgrass1 <dcs0036@mix.wvu.edu>
2021-09-03 15:37:12 -0400

    dispatch layout created and played in main layout with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { TopNavBarComponent } from './header/top-nav-bar.component';
 import { ProductCard } from './product-cards/firstProdCard';
 import { MainLayout } from './Layout/main-layout';
 import { AppRoutingModule } from './app-routing.module';
-import { ShipToPgComponent } from './Pages/ship-to-pg.component';
 import { DispatchLayoutComponent } from './Layout/dispatch-layout.component';
 import { HomepageLayoutComponent } from './Layout/homepage-layout.component';
 import { NavigationComponent } from './header/navigation.component';
@@ -22,7 +21,6 @@ import { NavigationComponent } from './header/navigation.component';
     MainVideo,
     ProductCard,
     MainLayout,
-    ShipToPgComponent,
     DispatchLayoutComponent,
     HomepageLayoutComponent,
     NavigationComponent,

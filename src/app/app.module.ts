92d94a881a67a25677445690e7dc077936b3b1d0
DavidSnodgrass1 <dcs0036@mix.wvu.edu>
2021-09-02 22:55:37 -0400

    lab2 progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { MainVideo } from './Main_Video_Homepage/Main-video';
 
 import { SecondTopNavBarComponent } from './header/2nd-top-nav-bar-component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { ProductCard } from './product-cards/firstProdCard';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
     SecondTopNavBarComponent,
-    MainVideo
+    MainVideo,
+    ProductCard
   ],
   imports: [
     BrowserModule
---
+++ src/app/product-cards/firstProdCard.ts
@@ -0,0 +1,10 @@
+import { Component, Type } from "@angular/core";
+
+@Component({
+    selector:'app-product-card',
+    templateUrl: 'firstProdCard.html',
+    styleUrls: ['./firstProdCard.css']
+})
+export class ProductCard {
+
+}

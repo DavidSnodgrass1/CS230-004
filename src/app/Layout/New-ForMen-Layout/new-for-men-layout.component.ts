30176964c0e06eec2c5889eabb78ad1512ecea5c
DavidSnodgrass1 <dcs0036@mix.wvu.edu>
2021-09-03 22:10:35 -0400

    Revert "added and sorted things"
    This reverts commit 4d2f40cb6a7e6a82defe94c0ea7d4cecde4a1cf3.

--- src/app/Layout/New-ForMen-Layout/new-for-men-layout.component.ts
+++ src/app/Layout/New-ForMen-Layout/new-for-men-layout.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-formen-layout',
   templateUrl: './new-for-men-layout.component.html',
-  styleUrls: ["./new-for-men-layout.component.css"]
 })
 export class NewForMenLayoutComponent implements OnInit {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { HelpLayoutComponent } from './Layout/Help-Layout-Folder/help-layout.com
 import { SustainLayoutComponent } from './Layout/Sustain-Layout-Folder/sustain-layout.component';
 import { StoresLayoutComponent } from './Layout/Stores-Layout-Folder/stores-layout.component';
 import { NewForMenLayoutComponent } from './Layout/New-ForMen-Layout/new-for-men-layout.component';
-import { ProductCardComponent } from './Body/product-card.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +30,6 @@ import { ProductCardComponent } from './Body/product-card.component';
     SustainLayoutComponent,
     StoresLayoutComponent,
     NewForMenLayoutComponent,
-    ProductCardComponent,
   ],
   imports: [
     BrowserModule,

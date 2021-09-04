8eff8540837cb53112981fe50e0e70be54c36cab
DavidSnodgrass1 <dcs0036@mix.wvu.edu>
2021-09-03 22:23:19 -0400

    added new layout component for the new mens page and adde routing to it

--- src/app/Layout/New-ForMen-Layout/new-for-men-layout.component.ts
+++ src/app/Layout/New-ForMen-Layout/new-for-men-layout.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-formen-layout',
   templateUrl: './new-for-men-layout.component.html',
+  styleUrls: ["./new-for-men-layout.component.css"]
 })
 export class NewForMenLayoutComponent implements OnInit {
 

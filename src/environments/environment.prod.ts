ebf8ee64755ecdcfc1eac8eb0ea55f600c9fb50b
DavidSnodgrass1 <dcs0036@mix.wvu.edu>
2021-10-17 21:35:06 -0400

    forgot to push the rest of lab 9

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAZr2LytOVuNOIhPZTVjZ6ZiFxT0IYZ63o",
+    authDomain: "lv-app-fac1a.firebaseapp.com",
+    databaseURL: "https://lv-app-fac1a-default-rtdb.firebaseio.com",
+    projectId: "lv-app-fac1a",
+    storageBucket: "lv-app-fac1a.appspot.com",
+    messagingSenderId: "684698169100",
+    appId: "1:684698169100:web:c69d5e62c718d3180d6329",
+    measurementId: "G-NG350JD422"
+  },
 };

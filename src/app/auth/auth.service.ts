190ce06784ee5c903a72e97bb75af9bedd24a71a
DavidSnodgrass1 <dcs0036@mix.wvu.edu>
2021-11-08 07:27:13 -0500

    finished lab 10

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,20 +7,28 @@ import { AuthResponse } from "./authResponse";
     providedIn: 'root'
 })
 export class AuthService{
-    baseUrl:string = "https://identitytoolkit.googleapis.com/v1/accounts";
-    signUpEndpoint:string = "signUp";
+    baseUrl:string = "https://identitytoolkit.googleapis.com/v1/accounts"
+    signUpEndpoint:string = "signUp"
+    signInEndpoint:string = "signInWithPassword"
 
     public constructor(private http:HttpClient){
     }
 
     public signup(email:string, password:string){
-
         const requestBody ={
             "email":email,
             "password":password,
             "returnSecureToken": true
         }
-
         return this.http.post<AuthResponse>(this.baseUrl+ ':' + this.signUpEndpoint + '?' + 'key=' + environment.firebase.apiKey,requestBody)
+    }
+
+    public login(email:string, password:string){
+        const requestBody ={
+            "email":email,
+            "password":password,
+            "returnSecureToken": true
+        }
+        return this.http.post<AuthResponse>(this.baseUrl + ':' + this.signInEndpoint + '?' + 'key=' + environment.firebase.apiKey, requestBody)
     }
 }
